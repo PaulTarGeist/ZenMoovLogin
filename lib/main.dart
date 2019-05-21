08e29071d700995f5071b093baca5e4194485243
paul <paul.tardivel@epitech.eu>
2019-05-21 15:46:40 +0200

    Modif: title name for the MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Login UI',
+      title: 'ZenMoov Login UI',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
